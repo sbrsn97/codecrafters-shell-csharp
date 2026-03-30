156fefa088da365bbaf4a1085f904fe9695ecd89
Sabri <sabrisen97@gmail.com>
2026-03-30 20:00:50 +0300

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -3,5 +3,7 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+        string? userInput = Console.ReadLine();
+        Console.WriteLine($"{userInput}: command not found");
     }
 }
