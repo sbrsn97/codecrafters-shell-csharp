2aca25ba2f947bf00a5a24c5c953be8b09b6222d
Sabri <sabrisen97@gmail.com>
2026-04-03 21:21:26 +0300

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 class Program
 {
     static void Main()
@@ -14,9 +17,11 @@ class Program
             if(userInput == null)
                 break;
                 
-            string[] splittedInput = userInput.Split(' ');
-            string command = splittedInput[0];
-            userInput = userInput.Substring(command.Length).Trim();
+            string[] splitInput = userInput.Split(new[] { ' ' }, 2);
+            string command = splitInput[0];
+
+            if(splitInput.Length > 1)
+                userInput = splitInput[1];
 
             switch(command)
             {
@@ -42,40 +47,77 @@ class Program
             if(defined.Any(x => x == input))
                 Console.WriteLine($"{input} is a shell builtin");
             else
-                //Console.WriteLine($"{input}: not found");
                 SearchForExecutables(input);
         }
 
         static void SearchForExecutables(string input)
         {
+            bool found = false;
+            string args = "";
             string? path = Environment.GetEnvironmentVariable("PATH");
             string[] directories = path!.Split(Path.PathSeparator);
-            bool found = false;
+            string[] splitInput = input.Split(new[] { ' '}, 2);
+            input = splitInput[0];
 
-            foreach(string directory in directories)
+            if(splitInput.Length > 1)
+                args = splitInput[1];
+
+            foreach (string directory in directories)
             {
                 string fullPath = Path.Combine(directory, input);
 
-                if(!File.Exists(fullPath))
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    continue;
+                    string[] extensions = { ".exe", ".bat", ".cmd", ".com" };
+                    foreach (var ext in extensions)
+                    {
+                        string candidate = fullPath + ext;
+                        if (File.Exists(candidate))
+                        {
+                            Execute(candidate, args);
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (File.Exists(fullPath))
+                    {
+                        var mode = File.GetUnixFileMode(fullPath);
+                        bool isExecutable = mode.HasFlag(UnixFileMode.UserExecute) ||
+                                            mode.HasFlag(UnixFileMode.GroupExecute) ||
+                                            mode.HasFlag(UnixFileMode.OtherExecute);
+
+                        if (isExecutable)
+                        {
+                            Execute(fullPath, args);
+                            found = true;
+                        }
+                    }
                 }
 
-                UnixFileMode mode = File.GetUnixFileMode(fullPath);
-
-                bool isExecutable = mode.HasFlag(UnixFileMode.GroupExecute) ||
-                                    mode.HasFlag(UnixFileMode.OtherExecute) ||
-                                    mode.HasFlag(UnixFileMode.UserExecute);
-
-                if(isExecutable)
-                {
-                    Console.WriteLine($"{input} is {fullPath}");
-                    found = true;
-                }
+                if (found) break;
             }
-            
-            if(!found)
+
+            if (!found)
                 Console.WriteLine($"{input}: not found");
+        }
+
+        static void Execute(string exePath, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.Arguments = args; 
+            start.FileName = exePath;
+
+            int exitCode;
+
+            using (Process proc = Process.Start(start)!)
+            {
+                proc.WaitForExit();
+
+                exitCode = proc.ExitCode;
+            }
         }
     }
 }
