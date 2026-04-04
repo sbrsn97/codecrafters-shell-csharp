2c7cfe69865528b1f17f5c271c5499ef4a9aec47
Sabri <sabrisen97@gmail.com>
2026-04-04 13:07:10 +0300

    codecrafters submit [skip ci]

--- src/BuiltinCommands.cs
+++ src/BuiltinCommands.cs
@@ -11,7 +11,7 @@ public static class BuiltinCommands
             ["exit"] = arg => Environment.Exit(0),
             ["pwd"]  = arg => Console.WriteLine(Directory.GetCurrentDirectory()),
             ["type"] = arg => PrintCommandType(arg!),
-            ["cd"]   = arg => Directory.SetCurrentDirectory(arg!)
+            ["cd"]   = arg => ChangeDirectory(arg!)
         };
 
     private static void PrintCommandType(string input)
@@ -20,5 +20,17 @@ public static class BuiltinCommands
             Console.WriteLine($"{input} is a shell builtin");
         else
             ExternalCommands.SearchForExecutables(input, false);
+    }
+
+    private static void ChangeDirectory(string path)
+    {
+        try
+        {
+            Directory.SetCurrentDirectory(path);
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("Unhandled exception. System.IO.DirectoryNotFoundException: Could not find a part of the path '/non-existing-directory'.");
+        }
     }
 }
